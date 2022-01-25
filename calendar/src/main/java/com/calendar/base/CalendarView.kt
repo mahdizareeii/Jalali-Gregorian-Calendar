ce5114e7ce48eedf034213f766811b573b1ff8da
M.Zareei <m.zareei@jabama.com>
2022-01-25 17:27:35 +0330

    fix bug

--- calendar/src/main/java/com/calendar/base/CalendarView.kt
+++ calendar/src/main/java/com/calendar/base/CalendarView.kt
@@ -8,8 +8,8 @@ import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.calendar.R
 import com.calendar.base.adapter.month.MonthAdapter
+import com.calendar.base.calendar.BaseCalendar
 import com.calendar.base.calendar.MyGregorianCalendar
-import com.calendar.base.calendar.MyJalaliCalendar
 import com.calendar.base.model.MonthItem
 import com.calendar.base.model.YearItem
 
@@ -23,32 +23,32 @@ class CalendarView @JvmOverloads constructor(
     //TODO for horizontal add PagerSnapHelper().attachToRecyclerView(recyclerView)
     //TODO create xml for properties and get it in the init
 
-    private val jalaliCalendar = MyJalaliCalendar()
-    private val gregorianCalendar = MyGregorianCalendar()
+    private var calendar: BaseCalendar
 
     private val adapter: MonthAdapter = MonthAdapter()
     private var recyclerView: RecyclerView
-    private val months = ArrayList<MonthItem>()
+    private val monthsList = ArrayList<MonthItem>()
 
     init {
         inflate(context, R.layout.calendar, this)
         recyclerView = this.findViewById(R.id.rv_months)
 
-        initRecyclerView(LinearLayoutManager.VERTICAL)
+        //TODO init with MyGregorianCalendar or MyJalaliCalendar depend user input
 
+        calendar = MyGregorianCalendar()
+        initRecyclerView(LinearLayoutManager.VERTICAL)
         submitYears(
             listOf(
-                YearItem(gregorianCalendar, 2022)
+                YearItem(calendar, 2022)
             )
         )
     }
 
     fun submitYears(years: List<YearItem>) {
         years.forEach {
-            months.addAll(it.getMonthOfYear())
+            monthsList.addAll(it.getMonthOfYear())
         }
-        adapter.submitList(months)
-        adapter.notifyDataSetChanged()
+        adapter.submitList(monthsList)
     }
 
     private fun initRecyclerView(orientation: Int) {
