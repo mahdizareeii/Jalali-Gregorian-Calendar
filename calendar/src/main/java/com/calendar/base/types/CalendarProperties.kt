b3de4ee832167ebf8a7e597bd698e0c5882c901f
M.Zareei <m.zareei@jabama.com>
2022-01-31 17:16:41 +0330

    change selected text color

--- calendar/src/main/java/com/calendar/base/types/CalendarProperties.kt
+++ calendar/src/main/java/com/calendar/base/types/CalendarProperties.kt
@@ -3,15 +3,17 @@ package com.calendar.base.types
 import com.calendar.base.model.DayItem
 
 data class CalendarProperties(
-    var today:DayItem? = null,
+    var today: DayItem? = null,
 
     //for range selection
     var selectedCheckIn: DayItem? = null,
     var selectedCheckOut: DayItem? = null,
 
     //for multiple selection
+    val selectedMultipleDayItem: ArrayList<DayItem>? = null,
 
     //for single selection
+    var selectedSingle: DayItem? = null
 
     //for without selection
 )
--- calendar/src/main/java/com/calendar/base/types/CalendarType.kt
+++ calendar/src/main/java/com/calendar/base/types/CalendarType.kt
@@ -17,7 +17,7 @@ abstract class CalendarType {
     ) {
         context = viewHolder.itemView.context
         backgroundAvailability(viewHolder, dayItem, properties)
-        textColor(viewHolder, dayItem)
+        textColor(viewHolder, dayItem, properties)
         viewHolder.bgDay.visibility = dayItem.visibility
         viewHolder.txtDay.text = dayItem.day.toString()
     }
@@ -36,12 +36,18 @@ abstract class CalendarType {
     private fun textColor(
         viewHolder: DayViewHolder,
         dayItem: DayItem,
+        properties: CalendarProperties
     ) {
         viewHolder.txtDay.setTextColor(
             ContextCompat.getColor(
                 context,
-                if (dayItem.isHoliday) R.color.red
-                else R.color.secondary
+                when {
+                    dayItem.isHoliday -> R.color.red
+                    else -> {
+                        if (isSelected(dayItem, properties)) R.color.white
+                        else R.color.secondary
+                    }
+                }
             )
         )
     }
@@ -51,5 +57,26 @@ abstract class CalendarType {
         properties: CalendarProperties
     ): Boolean {
         return properties.today != null && dayItem >= properties.today!!
+    }
+
+    private fun isSelected(
+        dayItem: DayItem,
+        properties: CalendarProperties
+    ): Boolean {
+        if (properties.selectedCheckIn != null && properties.selectedCheckOut != null)
+            return dayItem >= properties.selectedCheckIn!! && dayItem <= properties.selectedCheckOut!!
+
+        if (properties.selectedCheckIn != null)
+            return dayItem == properties.selectedCheckIn
+
+        if (properties.selectedCheckOut != null)
+            return dayItem == properties.selectedCheckOut
+
+        if (properties.selectedSingle != null)
+            return properties.selectedSingle == dayItem
+
+        return properties.selectedMultipleDayItem?.any {
+            it == dayItem
+        } ?: false
     }
 }
