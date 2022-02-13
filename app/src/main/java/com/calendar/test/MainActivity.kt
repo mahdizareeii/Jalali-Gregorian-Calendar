178cf346e000d51fbf9b0c209b950d2b15afb457
M.Zareei <m.zareei@jabama.com>
2022-02-13 18:34:46 +0330

    clean up code

--- app/src/main/java/com/calendar/test/MainActivity.kt
+++ app/src/main/java/com/calendar/test/MainActivity.kt
@@ -25,15 +25,18 @@ class MainActivity : AppCompatActivity(), RangeSelectionListener {
                 availableFromToday = true,
                 calendarOrientation = VERTICAL,
                 showDaysPrice = true,
-                minDaysInRangeSelection = 2,
+                minDaysInRangeSelection = 1,
                 customDays = arrayListOf(
-                    DayItem(1400, 11, 26, 550000.0, false, false),
-                    DayItem(1400, 11, 27, 12300.0, true, false),
-                    DayItem(1400, 11, 28, 430000.0, true, true),
-                    DayItem(1400, 12, 5, 78893.0, false, false),
-                    DayItem(1400, 12, 6, 99999.0, false, false),
-                    DayItem(1400, 12, 7, 100000.0, false, false),
-                    DayItem(1400, 12, 8, 100000.0, false, true),
+                    DayItem(1400, 11, 26, 550000.0, isHoliday = false, isDisable = false),
+                    DayItem(1400, 11, 27, 12300.0, isHoliday = true, isDisable = false),
+                    DayItem(1400, 11, 28, 430000.0, isHoliday = true, isDisable = true),
+                    DayItem(1400, 12, 5, 78893.0, isHoliday = false, isDisable = false),
+                    DayItem(1400, 12, 6, 99999.0, isHoliday = false, isDisable = false),
+                    DayItem(1400, 12, 7, 100000.0, isHoliday = false, isDisable = false),
+                    DayItem(1400, 12, 8, 100000.0, isHoliday = false, isDisable = true),
+                    DayItem(1400, 12, 15, 456000.0, isHoliday = false, isDisable = true),
+                    DayItem(1401, 1, 1, 100000.0, isHoliday = false, isDisable = true),
+                    DayItem(1400, 12, 29, 500000.0, isHoliday = true, isDisable = false),
                 )
             )
         )
