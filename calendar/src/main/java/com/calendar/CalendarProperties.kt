f7f32b45f9e9fb3152ff408d5e00a84446676f78
M.Zareei <m.zareei@jabama.com>
2022-02-14 15:02:15 +0330

    clean up code

--- calendar/src/main/java/com/calendar/CalendarProperties.kt
+++ calendar/src/main/java/com/calendar/CalendarProperties.kt
@@ -8,10 +8,11 @@ import com.calendar.base.model.DayItem
 import com.calendar.base.types.CalendarType
 
 /**
- *  if the
- *  @property showDaysPrice
- *  be true the calendar will show prices that you gave from
- *  @property customDays
+ *  @property regionalType set regional type of calendar
+ *  @property calendarType set calendar view type
+ *  @property calendarOrientation set orientation of calendar (HORIZONTAL,VERTICAL)
+ *  @property showDaysPrice if be true the calendar will show prices that you gave from customDays
+ *  @property availabilityRule for decision of check days availability
  */
 data class CalendarProperties(
     val regionalType: RegionalType,
@@ -32,8 +33,6 @@ data class CalendarProperties(
 
     //for single selection
     var selectedSingle: DayItem? = null
-
-    //for without selection
 ) {
     internal fun txtPriceVisibility(currentItem: DayItem) =
         showDaysPrice && selectedCheckOut != currentItem
