aae968667b4cb33537080c9cc5e9a2b37bbaf26e
M.Zareei <m.zareei@jabama.com>
2022-03-07 15:09:36 +0330

    clean up code

--- calendar/src/main/java/com/calendar/types/rangeslelection/RangeSelection.kt
+++ calendar/src/main/java/com/calendar/types/rangeslelection/RangeSelection.kt
@@ -1,11 +1,27 @@
 package com.calendar.types.rangeslelection
 
+import android.content.Context
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.view.doOnLayout
 import com.calendar.CalendarProperties
 import com.calendar.R
-import com.calendar.adapter.day.viewholder.DayViewHolder
 import com.calendar.adapter.day.DaysAdapterListener
+import com.calendar.adapter.day.viewholder.DayViewHolder
+import com.calendar.calendar.RegionalType
 import com.calendar.model.Day
 import com.calendar.types.CalendarType
+import com.calendar.utils.DateUtil
+import com.calendar.utils.bubbleview.BubbleLayout
+import com.calendar.utils.bubbleview.BubblePopupHelper
+import com.calendar.utils.dp
 import com.calendar.utils.setBackgroundFromDrawable
 
 class RangeSelection(
@@ -20,7 +36,7 @@ class RangeSelection(
     ) -> Unit
         get() = { viewHolder, dayItem, properties, listener ->
             if (checkAvailability(viewHolder, dayItem, properties)) {
-                onDayClicked(properties, dayItem, rangeSelectionListener)
+                onDayClicked(viewHolder, properties, dayItem, rangeSelectionListener)
                 listener.onDaysNotifyDataSetChanged()
             }
         }
@@ -81,30 +97,161 @@ class RangeSelection(
     }
 
     private fun onDayClicked(
+        viewHolder: DayViewHolder,
         property: CalendarProperties,
         currentDay: Day,
         listener: RangeSelectionListener
     ) {
         property.apply {
-            if (selectedCheckIn == currentDay) {
-                selectedCheckIn = null
-                selectedCheckOut = null
-            } else if (isCheckOutSelect() && currentDay == selectedCheckOut && currentDay.isDisable) {
-                return
-            } else if (selectedCheckIn == currentDay || selectedCheckIn == null || isCheckOutSelect()) {
-                selectedCheckIn = currentDay
-                selectedCheckOut = null
-                listener.onCheckInSelected(selectedCheckIn!!)
-            } else {
-                if (selectedCheckIn!! > currentDay) {
-                    selectedCheckOut = selectedCheckIn
-                    selectedCheckIn = currentDay
-                    listener.onCheckInSelected(selectedCheckIn!!)
-                    listener.onCheckOutSelected(selectedCheckOut!!)
-                } else {
-                    selectedCheckOut = currentDay
-                    listener.onCheckOutSelected(selectedCheckOut!!)
+            when {
+                selectedCheckIn == currentDay -> {
+                    selectedCheckIn = null
+                    selectedCheckOut = null
                 }
+                isCheckOutSelect() && currentDay == selectedCheckOut && currentDay.isDisable -> {
+                    return
+                }
+                selectedCheckIn == currentDay || selectedCheckIn == null || isCheckOutSelect() -> {
+                    selectedCheckIn = currentDay
+                    selectedCheckOut = null
+                    listener.onCheckInSelected(selectedCheckIn!!)
+                    createToolTip(
+                        viewHolder.itemView,
+                        createMinNightText(property.minDaysInRangeSelection, viewHolder.context)
+                    )
+                }
+                else -> {
+                    if (selectedCheckIn!! > currentDay) {
+                        selectedCheckOut = selectedCheckIn
+                        selectedCheckIn = currentDay
+                        listener.onCheckInSelected(selectedCheckIn!!)
+                        listener.onCheckOutSelected(selectedCheckOut!!)
+                    } else {
+                        selectedCheckOut = currentDay
+                        listener.onCheckOutSelected(selectedCheckOut!!)
+                    }
+                    createToolTip(
+                        viewHolder.itemView,
+                        getCountText(getSelectedDays(property), viewHolder.context)
+                    )
+                }
+            }
+        }
+    }
+
+    private fun createMinNightText(minNight: Int, context: Context): CharSequence {
+        val spannableStringBuilder = SpannableStringBuilder()
+        val s1 = SpannableString("حداقل شب رزرو:")
+            .apply {
+//                setSpan(TypefaceSpan(context, R.font.iran_yekan), 0, length, 0)
+                setSpan(
+                    ForegroundColorSpan(ContextCompat.getColor(context, R.color.secondary)),
+                    0,
+                    length,
+                    0
+                )
+                setSpan(
+                    12,
+                    0,
+                    length,
+                    0
+                )
+            }
+        val s2 = SpannableString("$minNight شب")
+            .apply {
+//                setSpan(TypefaceSpan(context, R.font.iran_yekan_bold), 0, length, 0)
+                setSpan(
+                    ForegroundColorSpan(ContextCompat.getColor(context, R.color.secondary)),
+                    0,
+                    length,
+                    0
+                )
+                setSpan(
+                    14,
+                    0,
+                    length,
+                    0
+                )
+            }
+        spannableStringBuilder
+            .append(s1)
+            .append(" ")
+            .append(s2)
+
+        return spannableStringBuilder
+    }
+
+    private fun getCountText(count: Int, context: Context): CharSequence {
+        return SpannableString("$count شب اقامت")
+            .apply {
+//                setSpan(TypefaceSpan(context, R.font.iran_yekan_bold), 0, length, 0)
+                setSpan(
+                    ForegroundColorSpan(ContextCompat.getColor(context, R.color.secondary)),
+                    0,
+                    length,
+                    0
+                )
+                setSpan(
+                    14,
+                    0,
+                    length,
+                    0
+                )
+            }
+    }
+
+    private fun getSelectedDays(property: CalendarProperties): Int {
+        return if (property.regionalType == RegionalType.Jalali)
+            DateUtil.diffDaysJalali(
+                property.selectedCheckIn ?: return 0,
+                property.selectedCheckOut ?: return 0
+            )[1].toInt()
+        else
+            DateUtil.diffDaysGregorian(
+                property.selectedCheckIn ?: return 0,
+                property.selectedCheckOut ?: return 0
+            )[1].toInt()
+    }
+
+    private fun createToolTip(view: View, text: CharSequence) {
+        view.doOnLayout {
+            val parent = view.parent as ViewGroup
+            val bubbleLayout = LayoutInflater
+                .from(view.context)
+                .inflate(R.layout.date_tip_view, parent, false) as BubbleLayout
+            bubbleLayout.findViewById<TextView>(R.id.tv_text).text = text
+            val location = IntArray(2)
+            view.getLocationInWindow(location)
+            val x = location[0]
+            val y = location[1]
+            val displayWidth = view.context.resources.displayMetrics.widthPixels
+            val popupWindow = BubblePopupHelper.create(view.context, bubbleLayout)
+            val magicNumber = (view.width / 2).toFloat() - view.dp(4)
+
+            if (x < parent.width / 2) {
+                bubbleLayout.arrowPosition = magicNumber
+                popupWindow.showAtLocation(
+                    view,
+                    Gravity.START or Gravity.TOP,
+                    x,
+                    y - (view.height - (view.height / 4))
+                )
+            } else {
+                bubbleLayout.arrowPosition = bubbleLayout.apply {
+                    measure(
+                        View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                        View.MeasureSpec.makeMeasureSpec(
+                            (parent as? View)?.width ?: 100,
+                            View.MeasureSpec.AT_MOST
+                        )
+                    )
+                }.measuredWidth - magicNumber
+                popupWindow.showAtLocation(
+                    view,
+                    Gravity.END or Gravity.TOP,
+                    displayWidth - (x + view.width),
+                    y - (view.height - (view.height / 4))
+                )
             }
         }
     }
---
+++ calendar/src/main/java/com/calendar/utils/bubbleview/ArrowDirection.java
@@ -0,0 +1,40 @@
+package com.calendar.utils.bubbleview;
+
+/**
+ * Created by sudamasayuki on 16/06/14.
+ * https://github.com/MasayukiSuda/BubbleLayout
+ */
+public enum ArrowDirection {
+    LEFT(0),
+    RIGHT(1),
+    TOP(2),
+    BOTTOM(3),
+    // CENTER
+    LEFT_CENTER(4),
+    RIGHT_CENTER(5),
+    TOP_CENTER(6),
+    BOTTOM_CENTER(7),
+    // HORIZONTAL > RIGHT
+    TOP_RIGHT(8),
+    BOTTOM_RIGHT(9);
+
+
+    private int value;
+
+    ArrowDirection(int value) {
+        this.value = value;
+    }
+
+    public static ArrowDirection fromInt(int value) {
+        for (ArrowDirection arrowDirection : ArrowDirection.values()) {
+            if (value == arrowDirection.getValue()) {
+                return arrowDirection;
+            }
+        }
+        return LEFT;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
